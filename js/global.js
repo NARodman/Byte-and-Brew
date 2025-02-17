79943f321df19486133d7011e6edaf8d5b5f7d3e
NARodman <ninaalanier8@gmail.com>
2025-02-17 15:18:38 -0500

    Update global.js

--- js/global.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    let navbarToggler = document.querySelector(".navbar-toggler");
-    let navbarCollapse = document.querySelector("#navbarNav");
-
-    navbarToggler.addEventListener("click", function() {
-        navbarCollapse.classList.toggle("show");
-    });
-});
