16fc889e268ecf61d85da97b7b3ff277bbd82641
NARodman <ninaalanier8@gmail.com>
2025-02-18 22:11:13 -0500

    Update home.js

--- js/home.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const toggler = document.querySelector(".navbar-toggler");
-    const navCollapse = document.querySelector(".navbar-collapse");
-
-    toggler.addEventListener("click", function() {
-        navCollapse.classList.toggle("show");
-    });
-
-});
