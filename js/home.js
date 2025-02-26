f58aaacfce1336dcdfbce9c162956e8ab5d91990
NARodman <ninaalanier8@gmail.com>
2025-02-26 12:07:31 -0500

    Update home.js

--- js/home.js
+++ js/home.js
@@ -1,3 +1,14 @@
+window.addEventListener("scroll", function() {
+    const navbar = document.querySelector(".navbar");
+    if (window.scrollY > 50) {
+        navbar.classList.add("scrolled");
+    } else {
+        navbar.classList.remove("scrolled");
+    }
+});
+
+
+
 document.addEventListener('DOMContentLoaded', function() {
     let slideIndex = 0;
     const slides = document.querySelectorAll(".slide");
