7b614d630e867dc571ae188a35171c109004e6f1
NARodman <ninaalanier8@gmail.com>
2025-02-25 21:58:23 -0500

    Update home.js

--- js/home.js
+++ js/home.js
@@ -1,1 +1,13 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let slideIndex = 0;
+    const slides = document.querySelectorAll(".slide");
+    const totalSlides = slides.length;
+    const slider = document.querySelector(".hero-slider");
 
+    function moveSlides() {
+        slideIndex = (slideIndex + 1) % totalSlides;
+        slider.style.transform = `translateX(-${slideIndex * 100}%)`;
+    }
+
+    setInterval(moveSlides, 5000);
+});
